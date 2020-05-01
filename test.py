4df0dd3641240e316bb0d0650cda1d7a3b552acf
LeKiwiDeBx <tatareau@free.fr>
2020-05-01 18:13:11 +0200

    init

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*-
+"""
+Documentation
+=========
+Bla Bla Bla
+"""
+
+
+def whythis():
+
+    """Hello doc!
+    Commentaire
+    long de
+    chez long.
+    """
+
+print(whythis.__doc__)
